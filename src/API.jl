a7a6c5d73c31ef94a4cdd8046b41017559e6ca04
Pilwon Hur <pilwonhur@gmail.com>
2019-10-26 19:21:23 -0500

    test diff

--- src/API.jl
+++ src/API.jl
@@ -1,4 +1,4 @@
-HurGlobalTime = Array{Sym}(undef,1)
+HurGlobalTime = Sym(undef)
 HurGlobalRF = Array{Sym}(undef,0)
 HurGlobalGeneralizedCoordinates = Array{SymFunction}(undef,0)
 HurGlobalListTriads = Array{Sym}(undef,1,3)
@@ -7,7 +7,7 @@ HurGlobalDCM = Array{Sym}(undef,1,9)
 
 # push!(HurGlobalRF,n)
 HurGlobalListTriads[1,:]=[n1 n2 n3]
-HurGlobalTime[1]=t
+HurGlobalTime=t
 # HurGlobalTriadsConversion=[n1=>n1 n2=>n2 n3=>n3]
 HurGlobalDCM[1,:]=[1.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0 1.0]
 
@@ -31,7 +31,6 @@ macro HurDefineTime(x) # ... is the way to handle tuples in the argument.
 	push!(tmp.args,:($(esc(x))=$(esc(symbols(x))) ))
 	# push!(tmp.args,:($(esc(HurGlobalTime))=$(esc(Sym(x))) ))
 	# push!(tmp.args, :(push!($(esc(HurGlobalTime)),$(esc(x))))  )
-	# global HurGlobalTime=HurGlobalTime[2]
 	global HurGlobalTime=Sym(string(x))
 	return tmp;
 end
