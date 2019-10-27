a4770d778a30389a1194fbbca174dce87f4e33e8
Pilwon Hur <pilwonhur@gmail.com>
2019-10-26 22:22:05 -0500

    update for v0.2

--- src/API.jl
+++ src/API.jl
@@ -78,7 +78,7 @@ macro HurDefineRF(x...) # ... is the way to handle tuples in the argument.
 			global HurGlobalDCM=vcat(HurGlobalDCM,[1.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0 1.0]);
 			HurGlobalDCM[1,:]=[1.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0 1.0];
 
-			global push!(HurGlobalAngularVel,0);
+			push!(HurGlobalAngularVel,0);
 
 			global HurGlobalListTriads=vcat(HurGlobalListTriads,[n1 n2 n3]);
 			n,=size(HurGlobalListTriads);
