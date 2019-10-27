f51143ddafd3fb7c6f624582480eec82e9d5780f
Pilwon Hur <pilwonhur@gmail.com>
2019-10-26 19:03:30 -0500

    dcm

--- src/API.jl
+++ src/API.jl
@@ -29,7 +29,7 @@ macro HurDefineTime(x) # ... is the way to handle tuples in the argument.
 	tmp=Expr(:block)
 
 	push!(tmp.args,:($(esc(x))=$(esc(symbols(x))) ))
-	push!(tmp.args,:($(esc(HurGlobalTime))=$(esc(Sym(x))) ))
+	# push!(tmp.args,:($(esc(HurGlobalTime))=$(esc(Sym(x))) ))
 	HurGlobalTime=Sym(string(x))
 	return tmp;
 end
